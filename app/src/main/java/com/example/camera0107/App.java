9bb9b99c04f6f3433c5363fba88245794cd15692
chunxia0202 <286358360@qq.com>
2024-05-13 01:32:34 +0800

    second commit mainactivity1

--- app/src/main/java/com/example/camera0107/App.java
+++ app/src/main/java/com/example/camera0107/App.java
@@ -2,6 +2,8 @@ package com.example.camera0107;
 import android.app.Application;
 public class App extends Application {
     private static App mInstance;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
